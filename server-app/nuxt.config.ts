828a72230aa1af1cd2bb51c6f8f3c88db82c70a7
kochizufan <kochizufan@gmail.com>
2024-09-05 11:19:53 +0900

    ソーシャルログイン

--- server-app/nuxt.config.ts
+++ server-app/nuxt.config.ts
@@ -1,5 +1,35 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: [
+    '@nuxtjs/auth-next' // authモジュール
+  ],
+  axios: {
+    baseURL: 'http://localhost:3000', // APIサーバーのベースURL
+  },
+  auth: {
+    strategies: {
+      google: {
+        clientId: 'YOUR_GOOGLE_CLIENT_ID', // Google OAuth クライアントIDを設定
+        codeChallengeMethod: '',
+        responseType: 'token id_token',
+        redirectUri: 'http://localhost:3000/login', // Googleに戻るURLを設定
+      },
+      github: {
+        clientId: 'YOUR_GITHUB_CLIENT_ID', // GitHub OAuth クライアントID
+        clientSecret: 'YOUR_GITHUB_CLIENT_SECRET',
+      },
+      twitter: {
+        clientId: 'YOUR_TWITTER_CLIENT_ID', // Twitter OAuth クライアントID
+        clientSecret: 'YOUR_TWITTER_CLIENT_SECRET',
+      }
+    },
+    redirect: {
+      login: '/login',   // ログインが必要な場合のリダイレクト先
+      logout: '/',       // ログアウト後のリダイレクト先
+      callback: '/login', // OAuthのコールバックURL
+      home: '/'          // ログイン後のリダイレクト先
+    }
+  },
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true }
 })
