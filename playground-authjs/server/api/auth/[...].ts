5346e5e95f4fa0c47ee4efdd30873a612af0fac2
kochizufan <kochizufan@gmail.com>
2024-09-11 14:07:17 +0900

    Add Twitter, Google, Facebook

--- playground-authjs/server/api/auth/[...].ts
+++ playground-authjs/server/api/auth/[...].ts
@@ -1,15 +1,23 @@
 import CredentialsProvider from 'next-auth/providers/credentials'
 import GithubProvider from 'next-auth/providers/github'
+import TwitterProvider from 'next-auth/providers/twitter'
+import GoogleProvider from 'next-auth/providers/google'
+import FacebookProvider from 'next-auth/providers/facebook'
 import { NuxtAuthHandler } from '#auth'
 
 export default NuxtAuthHandler({
   // a) Never hardcode your secret in your code!! and b) use a secure secret, `test-123` is **not** secure!!
   secret: process.env.AUTH_SECRET ?? 'test-123',
   providers: [
-    // @ts-expect-error You need to use .default here for it to work during SSR. May be fixed via Vite at some point
+    /*// @ts-expect-error You need to use .default here for it to work during SSR. May be fixed via Vite at some point
     GithubProvider.default({
       clientId: process.env.GITHUB_CLIENT_ID ?? 'your-client-id',
       clientSecret: process.env.GITHUB_CLIENT_SECRET ?? 'your-client-secret'
+    }),*/
+    // @ts-expect-error You need to use .default here for it to work during SSR. May be fixed via Vite at some point
+    GoogleProvider.default({
+      clientId: process.env.GOOGLE_CLIENT_ID ?? 'your-client-id',
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? 'your-client-secret'
     }),
     // @ts-expect-error You need to use .default here for it to work during SSR. May be fixed via Vite at some point
     CredentialsProvider.default({
