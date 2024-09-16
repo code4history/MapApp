f3fab72af75ca510cc85a44385a8b5aef6550451
Kohei Otsuka <kochizufan@gmail.com>
2024-09-16 09:03:22 +0900

    Add Styles for signin

---
+++ my-auth-app/tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+
+const config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        primary: '#1da1f2', // Twitterのブルー
+        secondary: '#db4437', // Googleのレッド
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
