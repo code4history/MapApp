1610e1295c90fa54fa718729fc7ff47498c46039
Kohei Otsuka <kochizufan@gmail.com>
2024-09-20 08:49:57 +0900

    Update image to data scheme

--- my-auth-app/lib/custom_prisma_adaptor.ts
+++ my-auth-app/lib/custom_prisma_adaptor.ts
@@ -5,13 +5,23 @@ import fetch from 'node-fetch';
 // CustomPrismaAdapter の定義
 const CustomPrismaAdapter = PrismaAdapter(prisma);
 
+function arrayBufferToBinaryString(arrayBuf: ArrayBuffer) {
+  let binaryString = "";
+  const bytes = new Uint8Array(arrayBuf);
+  const len = bytes.byteLength;
+  for (let i = 0; i < len; i++) {
+    binaryString += String.fromCharCode(bytes[i]);
+  }
+  return binaryString
+}
+
 async function fetchImageAsDataUrl(url) {
     try {
       // URLから画像を取得
       const response = await fetch(url);
       const buffer = await response.arrayBuffer();
       // 画像をBase64エンコードしてdata URLに変換
-      const dataUrl = `data:${response.headers.get('content-type')};base64,${buffer.toString()}`;
+      const dataUrl = `data:${response.headers.get('content-type')};base64,${btoa(arrayBufferToBinaryString(buffer))}`;
       return dataUrl;
     } catch (error) {
       console.error('画像の取得に失敗しました:', error);
@@ -25,8 +35,8 @@ CustomPrismaAdapter.createUser = async (user) => {
   if (!user.name) {
     user.name = user.defaultName; // jwtコールバックで設定されたデフォルト名を使用
   }
-  if (!user.image) {
-    user.image = await fetchImageAsDataUrl(user.defaultPicture); // jwtコールバックで設定されたデフォルト画像を使用
+  if (user.image?.match(/^http/)) {
+    user.image = await fetchImageAsDataUrl(user.image); // jwtコールバックで設定されたデフォルト画像を使用
   }
 
 
